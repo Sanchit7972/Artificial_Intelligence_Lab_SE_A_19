870f3a0e7208eec1c0a1ed9e529f878c25395eb5
Sanchit7972 <sanchit.hendre24@mmit.edu.in>
2025-10-10 09:51:12 +0530

    sanchit

---
+++ Artificial_Intelligence_Lab_SE_A_19/if.py
@@ -0,0 +1,9 @@
+# Example: Checking if a number is positive or negative
+
+number = int(input("Enter a number: "))
+
+if number >= 0:
+    print("The number is positive.")
+else:
+    print("The number is negative.")
+
