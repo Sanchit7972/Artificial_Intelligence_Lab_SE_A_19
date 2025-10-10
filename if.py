5919b83c3113582ecce16935360817b5dfc024ac
Sanchit7972 <sanchit.hendre24@mmit.edu.in>
2025-10-10 09:49:48 +0530

    Delete if.py

--- if.py
+++
@@ -1,9 +0,0 @@
-# Example: Checking if a number is positive or negative
-
-number = int(input("Enter a number: "))
-
-if number >= 0:
-    print("The number is positive.")
-else:
-    print("The number is negative.")
-
